b2fedabd35b21f80e817976188d006bee6c5ccc7
jlizquierdo <jlizquierdo@jlizquierdo.com>
2024-09-07 18:55:39 +0200

    Organización por carpetas y modelos de Provincia y Taller

---
+++ Modelo/Provincia.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redTaller.Modelo
+{
+    internal class Provincia
+    {
+
+        private String codigo;
+        private String nombre;
+
+        public Provincia() { }
+
+        
+        /// <summary>
+        /// Constructor de Provincia
+        /// </summary>
+        /// <param name="codigo">Código de Provincia</param>
+        /// <param name="nombre">Nombre</param>
+        public Provincia(string codigo, string nombre)
+        {
+            this.Codigo = codigo;
+            this.Nombre = nombre;
+        }
+
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+
+    }
+
+}
---
+++ Modelo/Taller.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace redTaller.Modelo
+{
+    /// <summary>
+    /// Clase Taller
+    /// </summary>
+    internal class Taller
+    {
+        private String nif;
+        private String nombre;
+        private String domicilio;
+        private String codigoPostal;
+        private String poblacion;
+        private String provincia;
+        private String movil;
+        private String telefono;
+        private String email;
+        private String web;
+
+        public Taller()
+        {
+        }
+
+        /// <summary>
+        /// Constructor Taller
+        /// </summary>
+        /// <param name="nif">Número de Identificación Fiscal</param>
+        /// <param name="nombre">Nombre</param>
+        /// <param name="domicilio">Domicilio</param>
+        /// <param name="codigoPostal">Código Postal</param>
+        /// <param name="poblacion">Población</param>
+        /// <param name="provincia">Provincia</param>
+        /// <param name="movil">Móvil</param>
+        /// <param name="telefono">Teléfono</param>
+        /// <param name="email">Dirección eMail</param>
+        /// <param name="web">URL Web</param>
+        public Taller(string nif, string nombre, string domicilio, string codigoPostal, string poblacion, string provincia, string movil, string telefono, string email, string web)
+        {
+            this.Nif = nif;
+            this.Nombre = nombre;
+            this.Domicilio = domicilio;
+            this.CodigoPostal = codigoPostal;
+            this.Poblacion = poblacion;
+            this.Provincia = provincia;
+            this.Movil = movil;
+            this.Telefono = telefono;
+            this.Email = email;
+            this.Web = web;
+        }
+
+        public string Nif { get => nif; set => nif = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Domicilio { get => domicilio; set => domicilio = value; }
+        public string CodigoPostal { get => codigoPostal; set => codigoPostal = value; }
+        public string Poblacion { get => poblacion; set => poblacion = value; }
+        public string Provincia { get => provincia; set => provincia = value; }
+        public string Movil { get => movil; set => movil = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Email { get => email; set => email = value; }
+        public string Web { get => web; set => web = value; }
+
+    }
+
+}
