a43908356e68db320e272a24b7eb4c3aeba26aa6
jlizquierdo <jlizquierdo@jlizquierdo.com>
2024-09-28 19:06:00 +0200

    Quitar using innecesarios

--- Database/Provincia/ProvinciaDB.cs
+++ Database/Provincia/ProvinciaDB.cs
@@ -1,12 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace redTaller.Database.Provincia
 {
