25b319176594de03a54b377b22465285594ca293
jlizquierdo <jlizquierdo@jlizquierdo.com>
2024-09-28 19:04:59 +0200

    Quitando using innecesarios

--- Controlador/ControladorProvincia.cs
+++ Controlador/ControladorProvincia.cs
@@ -1,14 +1,7 @@
 ﻿using redTaller.Database.Provincia;
 using redTaller.Modelo;
 using redTaller.Vista;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 
 namespace redTaller.Controlador
 {
