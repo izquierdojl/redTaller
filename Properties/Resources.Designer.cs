0d6f9b41697d972922ee9386eab7a48e3a14f1a5
jlizquierdo <jlizquierdo@jlizquierdo.com>
2024-10-26 17:06:08 +0200

    Vista Formulario Buscar

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace redTaller.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap aceptar {
+            get {
+                object obj = ResourceManager.GetObject("aceptar", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap anadir {
             get {
                 object obj = ResourceManager.GetObject("anadir", resourceCulture);
---
+++ Vista/VistaSearch/VistaListaSearch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace redTaller.Vista.VistaSearch
+{
+    public partial class VistaListaSearch : Form
+    {
+        public VistaListaSearch()
+        {
+            InitializeComponent();
+        }
+    }
+}
