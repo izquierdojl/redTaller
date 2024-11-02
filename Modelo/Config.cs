8e1807642333dcf977142a9545c359fddd6b12d9
jlizquierdo <jlizquierdo@jlizquierdo.com>
2024-11-02 16:49:00 +0100

    Ficheros de Configuración y Servicio de Cifrado

---
+++ Modelo/Config.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redTaller.Modelo
+{
+
+    public class Config
+    {
+        public string EmailCuenta { get; set; }
+        public string EmailUser { get; set; }
+        public string EmailPassword { get; set; }
+        public string EmailSmtpServer { get; set; }
+        public int EmailSmtpPort { get; set; }
+
+        public string MasterPassword { get; set; }
+
+        public Config() { }
+
+    }
+
+}
--- Vista/Principal.Designer.cs
+++ Vista/Principal.Designer.cs
@@ -31,11 +31,14 @@
             this.menuFicheros = new System.Windows.Forms.MenuStrip();
             this.ficherosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuFicherosTalleres = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFicherosClientes = new System.Windows.Forms.ToolStripMenuItem();
             this.menuFicherosProvincias = new System.Windows.Forms.ToolStripMenuItem();
             this.menuFicherosCodigosPostales = new System.Windows.Forms.ToolStripMenuItem();
             this.barStatus = new System.Windows.Forms.StatusStrip();
             this.barLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.menuFicherosClientes = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuHerramientas = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuConfiguración = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.menuFicheros.SuspendLayout();
             this.barStatus.SuspendLayout();
             this.SuspendLayout();
@@ -43,7 +46,8 @@
             // menuFicheros
             // 
             this.menuFicheros.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.ficherosToolStripMenuItem});
+            this.ficherosToolStripMenuItem,
+            this.menuHerramientas});
             this.menuFicheros.Location = new System.Drawing.Point(0, 0);
             this.menuFicheros.Name = "menuFicheros";
             this.menuFicheros.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
@@ -56,6 +60,7 @@
             this.ficherosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.menuFicherosTalleres,
             this.menuFicherosClientes,
+            this.toolStripSeparator1,
             this.menuFicherosProvincias,
             this.menuFicherosCodigosPostales});
             this.ficherosToolStripMenuItem.Name = "ficherosToolStripMenuItem";
@@ -69,6 +74,13 @@
             this.menuFicherosTalleres.Size = new System.Drawing.Size(222, 22);
             this.menuFicherosTalleres.Text = "Fichero de Talleres";
             this.menuFicherosTalleres.Click += new System.EventHandler(this.menuFicherosTalleres_Click);
+            // 
+            // menuFicherosClientes
+            // 
+            this.menuFicherosClientes.Name = "menuFicherosClientes";
+            this.menuFicherosClientes.Size = new System.Drawing.Size(222, 22);
+            this.menuFicherosClientes.Text = "Fichero de &Clientes";
+            this.menuFicherosClientes.Click += new System.EventHandler(this.menuFicherosClientes_Click);
             // 
             // menuFicherosProvincias
             // 
@@ -102,12 +114,25 @@
             this.barLabel1.Text = "redTaller";
             this.barLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // menuFicherosClientes
+            // menuHerramientas
             // 
-            this.menuFicherosClientes.Name = "menuFicherosClientes";
-            this.menuFicherosClientes.Size = new System.Drawing.Size(222, 22);
-            this.menuFicherosClientes.Text = "Fichero de &Clientes";
-            this.menuFicherosClientes.Click += new System.EventHandler(this.menuFicherosClientes_Click);
+            this.menuHerramientas.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuConfiguración});
+            this.menuHerramientas.Name = "menuHerramientas";
+            this.menuHerramientas.Size = new System.Drawing.Size(90, 19);
+            this.menuHerramientas.Text = "Herramientas";
+            // 
+            // menuConfiguración
+            // 
+            this.menuConfiguración.Name = "menuConfiguración";
+            this.menuConfiguración.Size = new System.Drawing.Size(180, 22);
+            this.menuConfiguración.Text = "Configuración";
+            this.menuConfiguración.Click += new System.EventHandler(this.menuConfiguración_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(219, 6);
             // 
             // Principal
             // 
@@ -141,6 +166,9 @@
         private System.Windows.Forms.ToolStripStatusLabel barLabel1;
         private System.Windows.Forms.ToolStripMenuItem menuFicherosTalleres;
         private System.Windows.Forms.ToolStripMenuItem menuFicherosClientes;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem menuHerramientas;
+        private System.Windows.Forms.ToolStripMenuItem menuConfiguración;
     }
 }
 
