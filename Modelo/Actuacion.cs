633a6961cc77f7a9657f43889eba76ba968c90ab
jlizquierdo <jlizquierdo@jlizquierdo.com>
2024-11-09 10:20:58 +0100

    Creación de modelos actuaciones

---
+++ Modelo/Actuacion.cs
@@ -0,0 +1,28 @@
+﻿using redCliente.Modelo;
+using System;
+
+namespace redTaller.Modelo
+{
+    internal class Actuacion
+    {
+
+        public int id { get; set; }
+        public Taller taller { get; set; }
+        public Cliente cliente { get; set; }
+        public Matricula Matricula { get; set; }
+        public DateTime fecha { get; set; }
+        public int km { get; set; }
+
+        public Actuacion(int id, Taller taller, Cliente cliente, Matricula matricula, DateTime fecha, int km)
+        {
+            this.id = id;
+            this.taller = taller;
+            this.cliente = cliente;
+            Matricula = matricula;
+            this.fecha = fecha;
+            this.km = km;
+        }
+
+    }
+
+}
---
+++ Modelo/ActuacionDetalle.cs
@@ -0,0 +1,19 @@
+﻿
+namespace redTaller.Modelo
+{
+    internal class ActuacionDetalle
+    {
+        public int id { get; set; }
+        public string id_reparacion { get; set; }
+        public string descripcion { get; set; }
+        public byte[] imagen { get; set; }
+        public ActuacionDetalle(int id, string id_reparacion, string descripcion, byte[] imagen)
+        {
+            this.id = id;
+            this.id_reparacion = id_reparacion;
+            this.descripcion = descripcion;
+            this.imagen = imagen;
+        }
+
+    }
+}
